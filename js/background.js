a1c7e61d221055d7416b33a9242faf5ae1ab7027
Walker Hildebrand <walker99113@gmail.com>
2019-03-18 19:05:18 -0400

    Simplify code

--- js/background.js
+++ js/background.js
@@ -6,8 +6,8 @@ chrome.runtime.onInstalled.addListener(function(details)
                 pageUrl: { hostEquals: '10fastfingers.com', schemes: ['https'] },
                 css: ["input[id='inputfield']"]
             })
-          ],
-          actions: [ new chrome.declarativeContent.ShowPageAction() ]
+        ],
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
     }
 
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function()
---
+++ js/content/tenfastfingers.js
@@ -0,0 +1,64 @@
+$(document).ready(() =>
+{
+    if (
+        document.URL.includes('10fastfingers.com/typing-test/') ||
+        document.URL.includes('10fastfingers.com/advanced-typing-test/') ||
+        document.URL.includes('10fastfingers.com/widgets/typingtest') ||
+        document.URL.includes('10fastfingers.com/competition/'))
+    {
+        cheat_typetest();
+    }
+    else if (document.URL.includes('10fastfingers.com/multiplayer'))
+    {
+        cheat_multiplayer();
+    }
+    else if (document.URL.includes('10fastfingers.com/anticheat/view/'))
+    {
+        cheat_anticheat();
+    }
+});
+
+
+const cheat_typetest = function()
+{
+    var inputfield = $('#inputfield');
+    inputfield.attr('placeholder', 'Spam spacebar to type real fast...');
+    inputfield.keydown((event) =>
+        {
+            if (event.key == " ")
+            {
+                var selectedWord = $('span[wordnr][class="highlight"]').text();
+                inputfield.val(selectedWord);
+                inputfield.focus();
+            }
+        }
+    );
+}
+
+const cheat_multiplayer = function()
+{
+    
+}
+
+const cheat_anticheat = function()
+{
+    var inputfield = $('#word-input');
+    var submitbutton = $('#submit-anticheat');
+    inputfield.attr('placeholder', 'Click start, and ten faster fingers will handle the typing for you...');
+
+    $('#start-btn').click(() =>
+    {
+        $('#word-img > img').on('load', () =>
+        {
+            var image = $('#word-img > img')[0];
+            Tesseract
+                    .recognize(image)
+                    .then(function(result){
+                        var text = (string)(result.text);
+                        console.log(text);
+                        inputfield.val(text);
+                        submitbutton.click();
+                    });
+        });
+    });
+}
